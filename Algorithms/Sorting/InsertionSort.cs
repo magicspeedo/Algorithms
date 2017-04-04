a2d8d021190467ba95df899c692d9545b2403860
magicspeedo <tyler.azevedo@Gmail.com>
2017-04-04 15:00:59 -0500

    doc

--- Algorithms/Sorting/InsertionSort.cs
+++ Algorithms/Sorting/InsertionSort.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
+    /// <summary>
+    /// O(n^2)
+    /// </summary>
     class InsertionSort : ISorter
     {
+        /// <summary>
+        /// Sorts the given array with a insertion sort
+        /// </summary>
+        /// <param name="array">The array to be sorted</param>
         public void Sort(int[] array)
         {
             for (int i = 1; i < array.Length; i++)
             {
-                int index = array[i]; int j = i;
+                int index = array[i]; 
+                int j = i;
                 while (j > 0 && array[j - 1] > index)
                 {
                     array[j] = array[j - 1];
